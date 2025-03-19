f037690fbb4561c10a01b8b88c91da80b59b4864
Victor Briaux <118478638+Malzware@users.noreply.github.com>
2025-03-19 03:59:15 +0100

    feat update v2

--- script.js
+++
@@ -1,9 +0,0 @@
-const cd = document.querySelector(".cd");
-
-cd.addEventListener("mouseover", () => {
-    cd.style.animationPlayState = "paused";
-});
-
-cd.addEventListener("mouseout", () => {
-    cd.style.animationPlayState = "running";
-});
