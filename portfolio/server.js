d32610bab47ac67ddb63937e8c5cc47c727f59dc
Victor Briaux <118478638+Malzware@users.noreply.github.com>
2025-03-23 00:04:51 +0100

    fest fix

--- portfolio/server.js
+++ portfolio/server.js
@@ -1,26 +1,29 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
 
 const app = express();
+
+// Définir le dossier contenant les fichiers générés par Vue
 const PORT = process.env.PORT || 8080;
 const DIST_DIR = path.join(__dirname, 'dist');
 
 // Servir les fichiers statiques
-app.use(express.static(DIST_DIR));
+app.use(express.static(DIST_DIR, {
+    setHeaders: function (res, path) {
+        if (path.endsWith('.js')) {
+            res.set('Content-Type', 'application/javascript');
+        } else if (path.endsWith('.css')) {
+            res.set('Content-Type', 'text/css');
+        }
+    }
+}));
 
-// Middleware pour vérifier si le fichier demandé existe avant de renvoyer index.html
+// Rediriger toutes les routes vers index.html (pour Vue Router en mode history)
 app.get('*', (req, res) => {
-  const filePath = path.join(DIST_DIR, req.url);
-
-  if (fs.existsSync(filePath) && !fs.lstatSync(filePath).isDirectory()) {
-    return res.sendFile(filePath);
-  }
-
-  res.sendFile(path.join(DIST_DIR, 'index.html'));
+    res.sendFile(path.join(DIST_DIR, 'index.html'));
 });
 
 // Démarrer le serveur
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
